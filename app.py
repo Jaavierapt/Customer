23866c688081d2980dfb144925f8bc492c7d2a97
Jaavierapt <javieraponce102@gmail.com>
2026-08-26 11:58:42 -0400

    Sincronizar ruta local de ingresos y evitar duplicados

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def generar_pdf(df_original):
 # 2. BLOQUE PRINCIPAL E INTERFAZ DE USUARIO CON STREAMLIT
 # =============================================================================
 if check_password():
-    RUTA_MAESTRA = os.path.abspath("Ingresos.xlsx")
+    RUTA_MAESTRA = r"C:\Users\office\Desktop\CRM web\Proyecto CRM\Ingresos.xlsx"
     ARCHIVO_CONTACTOS = "contactos.csv"
     ARCHIVO_TICKETS = "tickets_soporte.csv"
     ARCHIVO_HISTORIAL_INTERACCIONES = "historial_interacciones.csv"
